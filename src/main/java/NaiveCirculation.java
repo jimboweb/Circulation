a19b9cf2c699edcc41d4685a3a4ee2ddb6298959
jimboweb <jimboweb@gmail.com>
2018-04-22 21:28:42 -0400

    naive and test methods with algorithms planned

---
+++ src/main/java/NaiveCirculation.java
@@ -0,0 +1,27 @@
+/*
+    Algorithm for naive circulation:
+
+    choose a random node to begin
+    for each edge from random node
+        edge.flow = edge.minflow
+    for each edge with flow
+        edge.end demand -= edge.flow
+    for each node with -demand
+        if outgoingEdges empty
+            fail
+        for each outgoingEdge from node
+            if node.-demand < minFlow
+                call method to request flow back to beginning and fail if it goes over max flow or has no previous edges
+            outgoingEdge.flow = outgoingEdge.minFlow
+            node += outgoingEdge.minFlow
+        if node.demand != 0 fail
+    for each edge
+        if minFlow not met
+            call method to request demand from earlier nodes and fail if goes over max or has no previous edges
+    return success and flow for each edge
+
+ */
+
+
+public class NaiveCirculation {
+}
---
+++ src/test/java/CirculationTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/*
+    Algorithm to generate random min/max flow graphs
+
+    make a random number of nodes
+    4 out of 5 times
+        make a single circuit with random min and max flows
+    make sure each node has at least one edge coming out
+    add a random number of other max and min flow edges
+    return as input
+ */
+
+public class CirculationTest {
+
+    @Test
+    public void findCirculation() {
+    }
+}
