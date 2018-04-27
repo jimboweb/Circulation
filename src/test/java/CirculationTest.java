73066cc72b027c257cb1a2f0456bf3203daff0ad
jimboweb <jimboweb@gmail.com>
2018-04-27 09:08:25 -0400

    worked on test method

--- src/test/java/CirculationTest.java
+++ src/test/java/CirculationTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +18,34 @@ import static org.junit.Assert.*;
 public class CirculationTest {
 
     @Test
-    public void findCirculation() {
+    public void testFindCirculation() {
+        Circulation instance = new Circulation();
+
+    }
+
+    class TestInput implements Circulation.Inputter{
+        Random rnd;
+
+        private int n;
+
+        public TestInput(){
+            n = rnd.nextInt(20);
+            int[][] edges = new int[n][n];
+            int m = Math.random(n) + n;
+            for(int i=0;i<m;i++){
+                
+            }
+
+        }
+
+        @Override
+        public int getN() {
+            return n;
+        }
+
+        @Override
+        public int[][] getData() {
+            return new int[0][];
+        }
     }
 }
