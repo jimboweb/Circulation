e2a213b7a75f989148f11329a17aeded21fd4fb7
jimboweb <jimboweb@gmail.com>
2018-05-04 18:33:13 -0400

    NaiveCirculation coded
    almost certainly doesn't work

--- src/main/java/NaiveCirculation.java
+++ src/main/java/NaiveCirculation.java
@@ -1,28 +1,3 @@
-/*
-    Algorithm for naive circulation:
-
-    choose a random node to begin
-    for each edge from random node
-        edge.flow = edge.minflow
-    for each edge with flow
-        edge.end demand -= edge.flow
-    for each node with -demand
-        if outgoingEdges empty
-            fail
-        for each outgoingEdge from node
-            if node.-demand < minFlow
-                call method to request flow back to beginning and fail if it goes over max flow or has no previous edges
-            outgoingEdge.flow = outgoingEdge.minFlow
-            node += outgoingEdge.minFlow
-        if node.demand != 0 fail
-    for each edge
-        if minFlow not met
-            call method to request demand from earlier nodes and fail if goes over max or has no previous edges
-    return success and flow for each edge
-
- */
-
-
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
@@ -34,7 +9,7 @@ public class NaiveCirculation {
         int[] flows = new int[numEdges];
         int[][] edges = graph.getEdges();
         int[][] reverseEdgeLookup = getReverseEdgeLookup(graph, edges);
-        List<List<Integer>> prevEdges = getPrevEdges(numEdges);
+        ArrayList<ArrayList<Integer>> prevEdges = getPrevEdges(numEdges);
         int[][] minCapacities = graph.getMinCapacity();
         BitSet edgesSatisfied = new BitSet(numEdges);
         int currentEdgeNum = 0;
@@ -45,7 +20,7 @@ public class NaiveCirculation {
             int flow = graph.getMinCapacity(from, to);
             edgesSatisfied.set(currentEdgeNum);
             flows[currentEdgeNum] = flow;
-            if(!setNextEdges(flows, reverseEdgeLookup[to], prevEdges.get(currentEdgeNum), minCapacities[to],  flow)){
+            if(!setNextEdges(flows, reverseEdgeLookup[to], prevEdges.get(currentEdgeNum), minCapacities[to],  flow, prevEdges, minCapacities, edges)){
                 return new int[0];
             }
             currentEdgeNum=(currentEdgeNum+1)%numEdges;
@@ -53,11 +28,11 @@ public class NaiveCirculation {
         return flows;
     }
 
-    private boolean setNextEdges(int[] flows, int[] ints, List<Integer> integers, int[] minCapacity, int flow) {
+    private boolean setNextEdges(int[] flows, int[] ints, List<Integer> integers, int[] minCapacity, int flow, ArrayList<ArrayList<Integer>> prevEdges, int[][] minCapacities, int[][] edges) {
         for(int i = 0; i< minCapacity.length; i++){
             int demand = minCapacity[i];
             int nextEdge = ints[i];
-            flow = checkAndUpdateFlow(flow, demand, nextEdge);
+            flow = checkAndUpdateFlow(flow, demand, nextEdge,flows, prevEdges, minCapacities, edges);
             if(flow<0){
                     return false;
             }
@@ -68,20 +43,20 @@ public class NaiveCirculation {
         return true;
     }
 
-    private int checkAndUpdateFlow(int flow, int demand, int nextEdge) {
+    private int checkAndUpdateFlow(int flow, int demand, int nextEdge, int[] flows, ArrayList<ArrayList<Integer>> prevEdges, int[][] minCapacities, int[][] edges) {
         if(flow<demand){
             int newFlow = flow+demand;
-            if(increaseFlowBackwards(nextEdge,newFlow)){
+            if(getFlowFromPrevEdges(nextEdge, nextEdge,newFlow,flows, prevEdges, minCapacities, edges)==newFlow){
                 flow=newFlow;
             } else {
-                return new -1; //fail if previous min cpacities don't allow flow
+                return -1; //fail if previous min cpacities don't allow flow
             }
         }
         return flow;
     }
 
-    private List<List<Integer>> getPrevEdges(int numEdges) {
-        List<List<Integer>> prevEdges = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> getPrevEdges(int numEdges) {
+        ArrayList<ArrayList<Integer>> prevEdges = new ArrayList<>();
         for(int i=0;i<numEdges;i++){
             prevEdges.add(new ArrayList<>());
         }
@@ -98,9 +73,34 @@ public class NaiveCirculation {
         return reverseEdgeLookup;
     }
 
-    private boolean increaseFlowBackwards(int edgeNum, int newFlow){
-        // TODO: 5/1/18 cycle back through previous edges to get enough flow. will probably have to be recursive
-        // return true at 0, return false if you can't get the flow
-        return false;
+    private int getFlowFromPrevEdges(int firstEdge, int edgeNum, int newFlow, int[] flows, ArrayList<ArrayList<Integer>> prevEdges, int[][] minCapacities, int[][] edges){
+        //omg what a mess I'll have to comment the hell out of this the ugly way to know what I was doing
+        int thisEdgeFlow = flows[edgeNum];
+        int from = edges[edgeNum][0];
+        int to = edges[edgeNum][1];
+        int thisEdgeMinCapacity = minCapacities[from][to];
+        int availableFlow = thisEdgeMinCapacity-thisEdgeFlow;
+        if(availableFlow==0){ //stop here if we're out of flow
+            return 0;
+        }
+        int availFromPrevEdges = 0; //this will be the sum of all the flow we can get from the previous edges
+        List<Integer> prevEdge = prevEdges.get(edgeNum);
+        if(edgeNum==firstEdge) { // if the edgeNum is firstEdge we have a circuit and we're done
+            for (int e : prevEdge) {
+                availFromPrevEdges += getFlowFromPrevEdges(firstEdge,edgeNum, newFlow, flows, prevEdges, minCapacities, edges);
+                if (availFromPrevEdges >= newFlow) {
+                    availFromPrevEdges = newFlow;
+                    break;
+                }
+            }
+        }
+        newFlow=newFlow>availFromPrevEdges?newFlow:availFromPrevEdges;
+        if(availableFlow<newFlow){
+            flows[edgeNum]=thisEdgeMinCapacity;
+            return availableFlow;
+        } else {
+            flows[edgeNum]+=newFlow;
+            return newFlow;
+        }
     }
 }
